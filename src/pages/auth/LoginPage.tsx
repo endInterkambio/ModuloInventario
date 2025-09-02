b4af97d15eeaacc3111706b07ebe1fd68670544a
Enmanuel <end199615@gmail.com>
2025-09-02 10:02:47 -0500

    Custom login page

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -20,33 +20,33 @@ export const LoginPage = () => {
 
   return (
     <>
-      <h1 className="text-2xl font-semibold mb-4">Login</h1>
+      <h1 className="text-2xl font-semibold mb-4">Iniciar sesión</h1>
 
       <form onSubmit={ onSubmit }>
 
         <div className="mb-4">
-          <label className="block text-gray-600">Username</label>
+          <label className="block text-gray-600">Usuario</label>
           <input type="text" name="username" autoComplete="off" />
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-600">Password</label>
+          <label className="block text-gray-600">Contraseña</label>
           <input type="password" name="password" autoComplete="off" />
         </div>
 
         <div className="mb-4 flex items-center">
           <input type="checkbox" name="remember" className="text-blue-500" />
-          <label className="text-gray-600 ml-2">Remember Me</label>
+          <label className="text-gray-600 ml-2">Recuérdame</label>
         </div>
         
         <div className="mb-6 text-blue-500">
-          <a href="#" className="hover:underline">Forgot Password?</a>
+          <a href="#" className="hover:underline">¿Olvidaste tu contraseña?</a>
         </div>
 
-        <button type="submit" className="bg-indigo-600">Login</button>
+        <button type="submit" className="bg-[--color-primary] rounded-lg hover:bg-[--color-secondary] text-white hover:text-black">Ingresar</button>
       </form>
       <div className="mt-6 text-blue-500 text-center">
-        <a href="#" className="hover:underline">Sign up Here</a>
+        <a href="#" className="hover:underline">Registrate aquí</a>
       </div>
     </>
   );
